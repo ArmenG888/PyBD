9b78b84a7e57759c03e53d53c1c175c5b21ba5fe
ArmenG <52843550+ArmenG888@users.noreply.github.com>
2023-08-18 07:39:05 -0700

    Worked on andriod app

--- Backdoor/app/src/main/java/com/example/backdoor/MainActivity.kt
+++ Backdoor/app/src/main/java/com/example/backdoor/MainActivity.kt
@@ -13,7 +13,7 @@ import com.android.volley.toolbox.Volley
 class MainActivity : AppCompatActivity() {
     private var mRequestQueue: RequestQueue? = null
     private var mStringRequest: StringRequest? = null
-    private val url = "https://run.mocky.io/v3/85cf9aaf-aa4f-41bf-b10c-308f032f7ccc"
+    private val url = "http://127.0.0.1:8000/computer/1"
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext, "Response :$response", Toast.LENGTH_LONG)
                     .show() //display the response on screen
             }
-        ) { error -> Log.i(TAG, "Error :$error") }
+        ) { error -> Log.i("testtststs", "Error :$error") }
         mRequestQueue!!.add(mStringRequest)
     }
 }

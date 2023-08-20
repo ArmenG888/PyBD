e83ddb6035f8d36ab0a7bd9042bc6552a4a97fa3
ArmenG <52843550+ArmenG888@users.noreply.github.com>
2023-08-20 14:50:11 -0700

    worked on app

--- Backdoor/app/src/main/java/com/example/backdoor/MainActivity.kt
+++ Backdoor/app/src/main/java/com/example/backdoor/MainActivity.kt
@@ -9,11 +9,12 @@ import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     private var mRequestQueue: RequestQueue? = null
     private var mStringRequest: StringRequest? = null
-    private val url = "http://127.0.0.1:8000/computer/1"
+    private val url = "http://192.168.1.2:8000/computer/1"
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -28,6 +29,8 @@ class MainActivity : AppCompatActivity() {
         mStringRequest = StringRequest(
             Request.Method.GET, url,
             { response ->
+
+
                 Toast.makeText(applicationContext, "Response :$response", Toast.LENGTH_LONG)
                     .show() //display the response on screen
             }
